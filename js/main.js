f44ebf68d00e13842eef651d7d062e7ca8ba0819
Redger1 <87370440+Redger1@users.noreply.github.com>
2021-09-20 21:06:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,101 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for (var i = 0; i < 18; ++i) {
+	if (i == 11) {
+		var newColor = document.getElementById('a12');
+		newColor.style.backgroundColor = '#9447e1';
+	} else {
+		var newColor = document.getElementById(`${i + 1}`);
+		newColor.style.backgroundColor = getRandomColor();
+	}
+}
+
+
+
+
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("title");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+/* Переключатель цен */
+$(function () {
+    $('#switch-btn-1').click(function () {
+        $(this).toggleClass('switch-on');
+        if ($(this).hasClass('switch-on')) {
+            $(this).trigger('on.switch');
+        } else {
+            $(this).trigger('off.switch');
+        }
+    });
+    $('#switch-btn-1').on('on.switch', function () {
+        $('#block-1').addClass('bl-hide');
+        $('#block-2').removeClass('bl-hide');
+    });
+    $('.switch-btn').on('off.switch', function () {
+        $('#block-1').removeClass('bl-hide');
+        $('#block-2').addClass('bl-hide');
+    });
+});
+
+
+/* Accordion wrapper */
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
+}
